e3b2bc23b53a18e346b158aceef09193c0012aa6
parth <parthjohi009@gmail.com>
2024-05-31 17:54:58 +0530

    New

--- my-app/App.jsx
+++ my-app/App.jsx
@@ -1,50 +1,82 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import ChildComponentOne from './components/ChildComponentOne.jsx';
-import ChildTwo from './components/ChildTwo.jsx';
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import ChildComponentOne from "./components/ChildComponentOne.jsx";
+import ChildTwo from "./components/ChildTwo.jsx";
 
-
- const App = () =>  {
-
+const App = () => {
   const name = "Parth";
 
   const fun = () => {
-    alert("Hello Parth")
-  }
+    alert("Hello Parth");
+  };
   return (
     <View style={styles.container}>
       {/* <ChildComponentOne name={name}/>
       <ChildTwo fun={fun}/>
        <Text>Hello</Text> */}
-       <Text style={[styles.bgColor,styles.font,styles.text,styles.border]}>Hello World</Text>
-    </View>
-  )
+      {/* <Text style={[styles.bgColor,styles.font,styles.text,styles.border]}>Hello World</Text> */}
+      <View style={styles.box1}>
+      <Text style={styles.text1}>Hello</Text>
+      </View>
+      <View style={styles.box2}>
+      <Text style={styles.text2}>Hello</Text>
+      </View>
 
-}
+      <Image source={{ uri: "https://picsum.photos/200/300" }} style={{ width: 200, height: 100 ,marginTop: 20 , resizeMode:'center'}}></Image>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center"
   },
-  bgColor:{
-    backgroundColor: 'green',
-
+  bgColor: {
+    backgroundColor: "green"
   },
-  font:{
+  font: {
     fontSize: 40
   },
-  text:{
-    color: 'white'
+  text: {
+    color: "white"
   },
-  border:{
+  border: {
     borderWidth: 5,
-    borderColor: 'black',
+    borderColor: "black",
     borderRadius: 20,
     padding: 10
+  },
+  box1: {
+    backgroundColor: "green",
+    width: "50%",
+    height: "20%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    opacity: 0.4
+  },
+  text1:{
+    fontWeight: "bold",
+    fontSize: 40,
+    opacity: 0.9,
+    textShadowColor: "black",
+    textShadowRadius: 10,
+    textShadowOffset: { width: 2, height: 2 },
+  },
+  box2:{
+    backgroundColor: "red",
+    width: "50%",
+    height: "20%",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text2:{
+    fontWeight: "bold",
+    fontSize: 40,
   }
 
 });
-
-export default App 
+ 
+export default App;
